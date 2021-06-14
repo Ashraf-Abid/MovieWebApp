feba19babeef2fcbb792a0fea7c3cf22728d16d7
Ashraful Kalam Abid <abid7ctg@gmail.com>
2021-06-14 12:26:18 +0600

    added necessary function to make code DRY and customer and movie view added with details

--- helloWebapp/helloWebapp/Controllers/MoviesController.cs
+++ helloWebapp/helloWebapp/Controllers/MoviesController.cs
@@ -10,6 +10,21 @@ namespace helloWebapp.Controllers
 {
     public class MoviesController : Controller
     {
+        public ViewResult Index()
+        {
+            var movies = GetMovies();
+
+            return View(movies);
+        }
+
+        private IEnumerable<Movie> GetMovies()
+        {
+            return new List<Movie>
+            {
+                new Movie { Id = 1, Name = "Shrek" },
+                new Movie { Id = 2, Name = "Wall-e" }
+            };
+        }
         // GET: Movies
         //here it is returning Action result,though it is returning a view. But here we have written ActionResult.
         //We can also write it as public ViewResult 
@@ -38,14 +53,14 @@ namespace helloWebapp.Controllers
             //return RedirectToAction("Page","Controller");
             //return RedirectToAction("Index", "Home");
         }
-        public ActionResult Edit(int id) {
+       /* public ActionResult Edit(int id) {
             return Content("id=" + id);
-        }
+        }*/
         //Attribute routing
        // [Route("movies/released/{year}/{month:regex(\\d{4}:range(1,12)}")]
-        public ActionResult ByReleaseDate(int year,int month) {
+        /*public ActionResult ByReleaseDate(int year,int month) {
             return Content(year+"/"+month);
-        }         
+        } */        
         
     }
 }
