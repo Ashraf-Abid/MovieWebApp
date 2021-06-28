143050b2cfeab1d58fc49915c678380ca8a9fe73
Ashraful Kalam Abid <abid7ctg@gmail.com>
2021-06-29 00:25:41 +0600

    Added updating option for customer

--- helloWebapp/helloWebapp/Controllers/CustomerController.cs
+++ helloWebapp/helloWebapp/Controllers/CustomerController.cs
@@ -21,9 +21,18 @@ namespace helloWebapp.Controllers
         {
             _context.Dispose();
         }
+        public ActionResult Edit(int id) {
+            var customer = _context.customers.SingleOrDefault(c => c.Id == id);
+            if (customer == null) return HttpNotFound();
+            var viewModel = new CustomerformViewModel {
+                Customer = customer,
+                MembershipTypes = _context.membershiptypes.ToList()
+        };
+            return View("CustomerForm",viewModel);
+        }
         public ActionResult New() {
             var membershipTypes = _context.membershiptypes.ToList();
-            var vewmodel = new NewCustomerViewModel
+            var vewmodel = new CustomerformViewModel
             {
                 MembershipTypes = membershipTypes,
   
@@ -31,11 +40,26 @@ namespace helloWebapp.Controllers
             };
 
              
-            return View(vewmodel);
+            return View("CustomerForm",vewmodel);
         }
+
         [HttpPost]
-        public ActionResult Create(Customer customer) {
-            _context.customers.Add(customer);
+        public ActionResult Save(Customer customer) {
+            if (customer.Id == 0)
+            {
+                _context.customers.Add(customer);
+            }
+            else {
+                var customerInDb = _context.customers.Single(c => c.Id == customer.Id);
+                customerInDb.Name = customer.Name;
+
+                customerInDb.BirthDate = customer.BirthDate;
+                customerInDb.MembershipType = customer.MembershipType;
+                customerInDb.MembershipTypeId = customer.MembershipTypeId;
+                customerInDb.IsSubscribedToNewsletter = customer.IsSubscribedToNewsletter;
+
+
+            }
             _context.SaveChanges();
 
 
--- helloWebapp/helloWebapp/ViewModels/CustomerformViewModel.cs
+++ helloWebapp/helloWebapp/ViewModels/CustomerformViewModel.cs
@@ -6,7 +6,7 @@ using System.Web;
 using helloWebapp.Models;
 namespace helloWebapp.ViewModels
 {
-    public class NewCustomerViewModel
+    public class CustomerformViewModel
     {
         public IEnumerable<MembershipType> MembershipTypes { get; set; }
         public Customer Customer { get; set; } 
